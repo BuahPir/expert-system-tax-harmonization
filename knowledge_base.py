374e7bccff086710ffde80f6a22b91c9087fb674
BuahPir <andinadil90@gmail.com>
2026-04-28 21:02:58 +0800

    feat: modify question

--- knowledge_base.py
+++ knowledge_base.py
@@ -255,7 +255,7 @@ def get_next_question(answered: dict) -> str | None:
             if fid not in answered:
                 return fid
     else:
-        for fid in ["F03", "F04"]:
+        for fid in ["F03"]:
             if fid not in answered:
                 return fid
 
